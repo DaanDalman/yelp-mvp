bda36919faf4a952da9f252e22ff81bb978a2944
Daan Dalman <daandalman@gmail.com>
2021-02-16 20:47:58 +0100

    create new controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese", phone_number: 62478999}
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "japanese", phone_number: 62478999}
+bar_baarsch =  { name: "Bar Baarsch", address: "45 low str, Amsterdam 1011 AM", category: "belgian", phone_number: 62478999}
+
+[ dishoom, pizza_east, bar_baarsch ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
